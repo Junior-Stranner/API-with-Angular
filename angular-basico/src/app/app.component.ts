aea12b0c21fa8a76a28e966945148d88615dafd1
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-01 20:14:39 -0300

    add component 3 property binding

--- angular-basico/src/app/app.component.ts
+++ angular-basico/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Componente01Component } from "./componente01/componente01.component";
 import { Componente02Component } from "./componente02/componente02.component";
+import { Componente03Component } from "./componente03/componente03.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component]
+    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component, Componente03Component]
 })
 export class AppComponent {
   title = 'angular-basico';
