16bcd86a4941fbfd564710832682525076020608
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-01 19:46:15 -0300

    add component 1 for interploration on text and component 2 event blinding

--- angular-basico/src/app/app.component.ts
+++ angular-basico/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Componente01Component } from "./componente01/componente01.component";
+import { Componente02Component } from "./componente02/componente02.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component]
 })
 export class AppComponent {
   title = 'angular-basico';
