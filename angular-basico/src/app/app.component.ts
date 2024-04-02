139ed56447a2d2dc0ab02cacbd3ac94a8483ef72
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-02 16:16:02 -0300

    added Ractive Forms , so this is all what I did yesterday and today , now I gonna learning more to make a cool project

--- angular-basico/src/app/app.component.ts
+++ angular-basico/src/app/app.component.ts
@@ -9,13 +9,15 @@ import { Componente05Component } from "./componente05/componente05.component";
 import { Componente06Component } from "./componente06-Diretiva-ngSwitch/componente06.component";
 import { Componente07Component } from "./componente07-Diretiva-ngStyle-ngClass/componente07.component";
 import { Componente08Component } from "./componente08-Control-Flow-Condicional-For-switch/componente08.component";
+import { Componente09Component } from "./componente09-FormsModule/componente09.component";
+import { Componente10Component } from "./componente10-Reactive-Forms/componente10.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component, Componente08Component]
+    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component, Componente08Component, Componente09Component, Componente10Component]
 })
 export class AppComponent {
   title = 'angular-basico';
