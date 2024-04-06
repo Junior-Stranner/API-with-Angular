d52c686d8bd364a4dd925ca0686ae4e970a178ef
Junior-Stranner <Ju-stranner@outlook.com>
2024-04-06 09:09:09 -0300

    readme.md complete

--- Projeto-Angular/src/app/projeto-angular/projeto-angular.component.ts
+++ Projeto-Angular/src/app/projeto-angular/projeto-angular.component.ts
@@ -25,6 +25,9 @@ btnCadastrar:boolean = true;
 //Vetor 
 vetor:Pessoa[] = [];
 
+// Armazenar indice da Pessoa Selecionada
+   indice:number = -1;
+
 //Função de cadastro
 cadastrar(){
 
@@ -37,4 +40,62 @@ cadastrar(){
    //Visuabilidade via console
    console.table(this.vetor);
    }
+
+     // Função de seleção
+  selecionar(indice:number){
+
+    // Atribuir o índice da pessoa
+    this.indice = indice;
+
+    // Atribuir os dados da pessoa no formulário
+    this.formulario.setValue({
+      nome  : this.vetor[indice].nome,
+      idade : this.vetor[indice].idade,
+      cidade: this.vetor[indice].cidade
+    });
+
+    // Visibilidade dos botões
+    this.btnCadastrar = false;
+
+  }
+
+ // Função de alteração
+ alterar(){
+
+  // Alterar vetor
+  this.vetor[this.indice] = this.formulario.value as Pessoa;
+
+  // Limpar os inputs
+  this.formulario.reset();
+
+  // Visibilidade dos botões
+  this.btnCadastrar = true;
+
+}
+
+// Função de remoção
+  remover(){
+
+    // Removendo pessoa do vetor
+    this.vetor.splice(this.indice, 1);
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Visibilidade dos botões
+    this.btnCadastrar = true;
+
+  }
+
+  // Função de cancelamento
+  cancelar(){
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Visibilidade dos botões
+    this.btnCadastrar = true;
+
+  }
+
 }
